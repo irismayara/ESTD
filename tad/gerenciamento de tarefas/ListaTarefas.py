570b89ed67b762eb64e00ccb66b9b3e9637b49f5
Iris Mayara <mayaraim.114@gmail.com>
2025-03-14 16:29:29 -0300

    add

--- tad/gerenciamento de tarefas/ListaTarefas.py
+++ tad/gerenciamento de tarefas/ListaTarefas.py
@@ -4,6 +4,46 @@ class ListaTarefas:
         self.tamanho = 0
         self.tarefas = [None] * self.capacidade
 
+    def __str__(self):
+        string = '['
+
+        if self.tamanho > 0:
+            for t in self.tarefas: 
+                if t == None: break
+                string += str(t)
+                string += ', '
         
+        return string[: -2] + ']'
+    
+    def adicionar_no_fim(self, tarefa):
+        if self.tamanho < self.capacidade:
+            self.tarefas[self.tamanho] = tarefa
+        self.tamanho += 1
+
+    def adicionar_em_posicao(self, tarefa, posicao):
+        if posicao < 0 or posicao > self.tamanho:
+            print("Posição Inválida!")
+            return 
+        
+        if self.tamanho < self.capacidade:
+            novo_elemento = tarefa
+            for i in range(posicao, self.capacidade):
+                temp = self.tarefas[i]
+                self.tarefas[i] = novo_elemento
+                novo_elemento = temp
+            self.tamanho += 1
+
+    def obter_tarefa(self, posicao):
+        pass
+
+    def remover_tarefa(self, posicao):
+        pass
+
+    def contem(self, tarefa):
+        pass
+
+    def tamanho(self):
+        pass
+
 
     
--- tad/gerenciamento de tarefas/testeListaTarefas.py
+++ tad/gerenciamento de tarefas/testeListaTarefas.py
@@ -1,7 +1,90 @@
+from Tarefa import *
 from ListaTarefas import *
 
-lista = ListaTarefas()
+def criaListaVazia():
+    return ListaTarefas()
 
-print(lista.capacidade) # Saída: 10
-print(lista.tamanho) # Saída: 0
-print(lista.tarefas) # Saída: [None, None, None, None, None, None, None, None, None, None]
+def criaTarefas():
+    return [
+        Tarefa("Fazer a lista de TAD", "Média"), #0
+        Tarefa("Ler o artigo", "Média"), #1
+        Tarefa("Ler um livro", "Baixa"), #2
+        Tarefa("Resolver exercícios de Álgebra", "Alta"), #3
+        Tarefa("Revisar matéria para prova", "Alta"), #4
+        Tarefa("Estudar Python", "Média"), #5
+        Tarefa("Escrever relatório", "Média"), #6
+        Tarefa("Fazer compras", "Baixa"), #7
+        Tarefa("Assistir aula online", "Alta"), #8
+        Tarefa("Ler um artigo científico", "Média"), #9
+    ]
+
+def criaListaPreechida():
+    lista = criaListaVazia()
+    tarefas = criaTarefas()[:9]
+
+    for tarefa in tarefas:
+        lista.adicionar_no_fim(tarefa)
+
+    return lista
+
+def testeLista():
+    lista = criaListaVazia()
+
+    print(lista.capacidade) # Saída: 10
+    print(lista.tamanho) # Saída: 0
+    print(lista.tarefas) # Saída: [None, None, None, None, None, None, None, None, None, None]
+
+def testeAdicionarNoFinal():
+    lista = criaListaVazia()
+
+    tarefa1, tarefa2, tarefa3 = criaTarefas()[:3]
+
+    lista.adicionar_no_fim(tarefa1)
+    lista.adicionar_no_fim(tarefa2)
+    lista.adicionar_no_fim(tarefa3)
+
+    print(lista) 
+    print(lista.tamanho) # Saída: 3
+
+def testeAdicionarNoInicio():
+    lista = criaListaPreechida() # com 9 elementos
+    nova_tarefa = criaTarefas()[9]
+
+    ## adiciona no inicio, posicao: 0
+    lista.adicionar_em_posicao(nova_tarefa, 0)
+
+    print(lista)
+    print(lista.tamanho) # Saída 10
+
+def testeAdicionarNoMeio():
+    lista = criaListaPreechida() # com 9 elementos
+    nova_tarefa = criaTarefas()[9]
+
+    ## adiciona no meio, posicao: tamaho//2
+    lista.adicionar_em_posicao(nova_tarefa, lista.tamanho//2)
+
+    print(lista)
+    print(lista.tamanho) # Saída 10
+
+def testeAdicionarNoFim():
+    lista = criaListaPreechida() # com 9 elementos
+    nova_tarefa = criaTarefas()[9]
+
+    ## adiciona no fim, posicao: tamanho
+    lista.adicionar_em_posicao(nova_tarefa, lista.tamanho)
+
+    print(lista)
+    print(lista.tamanho) # Saída 10
+
+def testeAdicionarPosicaoInvalida():
+    lista = criaListaPreechida() # com 9 elementos
+    nova_tarefa = criaTarefas()[9]
+
+    ## adiciona em posicao invalida, posicao: 10
+    lista.adicionar_em_posicao(nova_tarefa, 10)
+
+    print(lista)
+    print(lista.tamanho) # Saída 9
+
+
+testeAdicionarPosicaoInvalida()
