ea4412c04e7100e9fc2a180e9b8f27f13ac0a638
Iris Mayara <mayaraim.114@gmail.com>
2025-03-23 16:28:39 -0300

    cria TAD Pilha

---
+++ tad/pilha/Pilha.py
@@ -0,0 +1,18 @@
+class Pilha:
+    def __init__(self):
+        self.pilha = []
+
+    def __str__(self):
+        pass
+
+    def push(self):
+        pass
+
+    def pop(self):
+        pass
+
+    def top(self):
+        pass
+
+    def isEmpty(self):
+        pass
